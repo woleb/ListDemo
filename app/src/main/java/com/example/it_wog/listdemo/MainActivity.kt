72fd5fa6179ba797cf3cd6f9da41bf4cee03a37d
Wolfgang <wolfgang.eberding@googlemail.com>
2018-10-22 14:29:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/it_wog/listdemo/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.it_wog.listdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val items = ArrayList<String>()
+        items.add("Tom")
+        items.add("Manu")
+        items.add("Julia")
+        items.add("Birgit")
+        items.add("Tom")
+        items.add("Manu")
+        items.add("Julia")
+        items.add("Birgit")
+        items.add("Tom")
+        items.add("Manu")
+        items.add("Julia")
+        items.add("Birgit")
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items )
+        kontaktListe.adapter = adapter
+/* Gibt eine Fehlermedlung da setOnClickListener nicht auf Adapter angewendet werden können
+        kontaktListe.setOnClickListener {
+
+            Toast.makeText(this, "", Toast.LENGTH_LONG).show()
+
+        }
+*/
+        kontaktListe.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, "Geklickt wurde: $position", Toast.LENGTH_LONG).show()
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/it_wog/listdemo/Person.kt
@@ -0,0 +1,7 @@
+package com.example.it_wog.listdemo
+
+data class Person (val name: String, val alter: Int, val note: String){
+
+
+
+}
