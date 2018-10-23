1f8714fe0b8019bb493544cb36f6d495dd1ed09c
IT-WOG <wolfgang.eberding@googlemail.com>
2018-10-23 18:00:36 +0200

    Bilder hinzugefügt

--- app/src/main/java/com/example/it_wog/listdemo/MainActivity.kt
+++ app/src/main/java/com/example/it_wog/listdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.it_wog.listdemo
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,18 +14,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val items = ArrayList<Person>()
-        items.add(Person("Tom", alter = 25, note = "Was", farbe = Color.parseColor("#ea1004")))
-        items.add(Person("Fritz", alter = 10, note = "Was", farbe = Color.parseColor("#77f442")))
-        items.add(Person("Joe", alter = 34, note = "Was", farbe = Color.parseColor("#ea1004")))
-        items.add(Person("Jim", alter = 4, note = "Was", farbe = Color.parseColor("#77f442")))
-        items.add(Person("Sue", alter = 23, note = "Was", farbe = Color.parseColor("#ea1004")))
-        items.add(Person("Nadja", alter = 56, note = "Was", farbe = Color.parseColor("#77f442")))
-        items.add(Person("Emma", alter = 43, note = "Was", farbe = Color.parseColor("#ea1004")))
-        items.add(Person("Birgit", alter = 25, note = "Was", farbe = Color.parseColor("#77f442")))
-        items.add(Person("Jakob", alter = 12, note = "Was", farbe = Color.parseColor("#ea1004")))
-        items.add(Person("Wolle", alter = 32, note = "Was", farbe = Color.parseColor("#77f442")))
-        items.add(Person("Ed", alter = 43, note = "Was", farbe = Color.parseColor("#ea1004")))
-
+        items.add(Person("Tom", alter = 25, note = "Was", farbe = Color.parseColor("#ea1004"), pic = getDrawable(R.drawable.pers1)))
+        items.add(Person("Fritz", alter = 10, note = "Was", farbe = Color.parseColor("#77f442"), pic = getDrawable(R.drawable.pers2)))
+        items.add(Person("Joe", alter = 34, note = "Was", farbe = Color.parseColor("#ea1004"), pic = getDrawable(R.drawable.pers3)))
+        items.add(Person("Jim", alter = 4, note = "Was", farbe = Color.parseColor("#77f442"), pic = getDrawable(R.drawable.pers1)))
+        items.add(Person("Sue", alter = 23, note = "Was", farbe = Color.parseColor("#ea1004"), pic = getDrawable(R.drawable.pers2)))
+        items.add(Person("Nadja", alter = 56, note = "Was", farbe = Color.parseColor("#77f442"), pic = getDrawable(R.drawable.pers3)))
+        items.add(Person("Emma", alter = 43, note = "Was", farbe = Color.parseColor("#ea1004"), pic = getDrawable(R.drawable.pers1)))
+        items.add(Person("Birgit", alter = 25, note = "Was", farbe = Color.parseColor("#77f442"), pic = getDrawable(R.drawable.pers2)))
+        items.add(Person("Jakob", alter = 12, note = "Was", farbe = Color.parseColor("#ea1004"), pic = getDrawable(R.drawable.pers3)))
+        items.add(Person("Wolle", alter = 32, note = "Was", farbe = Color.parseColor("#77f442"), pic = getDrawable(R.drawable.pers1)))
+        items.add(Person("Ed", alter = 43, note = "Was", farbe = Color.parseColor("#ea1004"), pic = getDrawable(R.drawable.pers2)))
+        Log.i("TEST", "Bildadresse ${getDrawable(R.drawable.pers1)}")
 
         //val adapter = ArrayAdapter<Person>(this, android.R.layout.simple_list_item_1, items )
         //kontakliste.adapter = adapter
