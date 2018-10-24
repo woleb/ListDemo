7e858014eb6269371d5f2efd7425b8afde09b7ec
Wolfgang <wolfgang.eberding@googlemail.com>
2018-10-24 06:38:16 +0200

    drawable hinzugefügt, und Android Version von 19 auf 21 geändert

--- app/src/main/java/com/example/it_wog/listdemo/Person.kt
+++ app/src/main/java/com/example/it_wog/listdemo/Person.kt
@@ -1,6 +1,8 @@
 package com.example.it_wog.listdemo
 
-data class Person (val name: String, val alter: Int, val note: String, val farbe: Int){
+import android.graphics.drawable.Drawable
+
+data class Person (val name: String, val alter: Int, val note: String, val farbe: Int, val pic: Drawable){
 
     override fun toString(): String {
         return "$name ($alter) Jahre"
