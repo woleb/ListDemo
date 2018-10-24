a9f94f5db4f685086e35221d1069d01eba8790d6
Wolfgang <wolfgang.eberding@googlemail.com>
2018-10-24 13:37:09 +0200

    Toolbar added (doesn't work like in the course example, no scrolling and two toolbars

--- app/src/main/java/com/example/it_wog/listdemo/DetailActivity.kt
+++ app/src/main/java/com/example/it_wog/listdemo/DetailActivity.kt
@@ -5,13 +5,14 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(toolbar_layout)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
@@ -23,6 +24,10 @@ class DetailActivity : AppCompatActivity() {
         val note: String = meinIntent.getStringExtra("userNote")
         val farbe: String = meinIntent.getStringExtra("userColor")
         Log.i("TEST", name + alter + note + farbe)
+
+        toolbar3.title = name + " " + alter
+        toolbar3.setBackgroundColor(farbe.toInt())
+        //contentText.text = note
     }
 
 
